0a346acbfe8554968bb8b77d31ebca9fed6f6725
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:21:23 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FAITHLESS ';
+export let nameSongs = [
+	"INSOMNIA", /*1*/
+	"WE COME 1", /*2*/
+	"GOD IS A DJ", /*3*/
+	"MASS DESTRUCTION", /*4*/
+	" DON’T LEAVE", /*5*/
+	"MUHAMMAD ALI", /*6*/
+	"REVERENCE", /*7*/
+	"SALVA MEA", /*8*/
+	"ONE STEP TOO FAR", /*9*/
+	"BRING MY FAMILY BACK", /*10*/
+	"MISS U LESS", /*11*/
+	"TARANTULA", /*12*/
+	"FATTY BOO", /*13*/
+	"REASONS", /*14*/
+	"WHY GO?", /*15*/
+	"I WANT MORE", /*16*/
+	"TAKE THE LONG WAY HOME", /*17*/
+	"IF LOVING YOU IS WRONG", /*18*/
+	"SHE'S MY BABY", /*19*/
+	"LIONTAMER", /*20*/
+]
